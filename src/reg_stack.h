cf89d726c31dc935e35ff646f53de1705aecf34a
Lenny2Liu <117865161+Lenny2Liu@users.noreply.github.com>
2025-03-07 18:47:45 -0600

    modify stack structure, move forloop

--- src/reg_stack.h
+++ src/reg_stack.h
@@ -12,65 +12,28 @@ namespace genetic {
  * @tparam MaxSize max capacity of the stack
  */
 template <typename DataT, int MaxSize> struct stack {
-  explicit stack() : elements_(0) {
-    for (int i = 0; i < MaxSize; ++i) {
-      regs_[i] = DataT(0);
-    }
-  }
+  explicit stack() : elements_(0) {}
 
-  /** Checks if the stack is empty */
-  bool empty() const { return elements_ == 0; }
+  bool empty() const { return (elements_ == 0); }
+  int size()  const { return elements_; }
+  bool full() const { return (elements_ == MaxSize); }
 
-  /** Current number of elements in the stack */
-  int size() const { return elements_; }
-
-  /** Checks if the number of elements in the stack equal its capacity */
-  bool full() const { return elements_ == MaxSize; }
-
-  /**
-   * @brief Pushes the input element to the top of the stack
-   *
-   * @param[in] val input element to be pushed
-   *
-   * @note If called when the stack is already full, then it is a no-op! To keep
-   *       the device-side logic simpler, it has been designed this way. Trying
-   *       to push more than `MaxSize` elements leads to all sorts of incorrect
-   *       behavior.
-   */
   void push(DataT val) {
-    for (int i = MaxSize - 1; i >= 0; --i) {
-      if (elements_ == i) {
-        ++elements_;
-        regs_[i] = val;
-      }
+    if (elements_ < MaxSize) {
+      regs_[elements_++] = val;
     }
   }
 
-  /**
-   * @brief Lazily pops the top element from the stack
-   *
-   * @return pops the element and returns it, if already reached bottom, then it
-   *         returns zero.
-   *
-   * @note If called when the stack is already empty, then it just returns a
-   *       value of zero! To keep the device-side logic simpler, it has been
-   *       designed this way. Trying to pop beyond the bottom of the stack leads
-   *       to all sorts of incorrect behavior.
-   */
   DataT pop() {
-    for (int i = 0; i < MaxSize; ++i) {
-      if (elements_ == (i + 1)) {
-        elements_--;
-        return regs_[i];
-      }
+    if (elements_ > 0) {
+      return regs_[--elements_];
     }
-
     return DataT(0);
   }
 
 private:
   int elements_;
   DataT regs_[MaxSize];
-}; // struct stack
+};
 
 } // namespace genetic
